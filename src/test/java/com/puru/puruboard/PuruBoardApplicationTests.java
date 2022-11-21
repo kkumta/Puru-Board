6943268aad883d3afbf3a711a42d931639c3a92c
Choi Dasom <dasomchoi52@gmail.com>
2022-11-22 00:03:21 +0900

    Create Spring Boot Application

---
+++ src/test/java/com/puru/puruboard/PuruBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puru.puruboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuruBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
