356ce73b19251071c9bb158fb42da334d3ced79b
Choi Dasom <dasomchoi52@gmail.com>
2022-11-28 11:37:04 +0900

    Use lambda in loadUserByUsername

--- src/main/java/com/puru/puruboard/security/service/CustomUserDetailsService.java
+++ src/main/java/com/puru/puruboard/security/service/CustomUserDetailsService.java
@@ -4,7 +4,6 @@ import com.puru.puruboard.domain.User;
 import com.puru.puruboard.domain.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -17,22 +16,18 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    
-        System.out.println("email: "+email);
-        Optional<User> user = userRepository.findByEmail(email);
         
-        if (user.isEmpty()) {
-            throw new UsernameNotFoundException("해당 email로 등록된 회원이 없습니다.");
-        }
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("해당 email로 등록된 회원이 없습니다."));
         
         List<GrantedAuthority> roles = new ArrayList<>();
-        roles.add(new SimpleGrantedAuthority(user.get().getRole()));
+        roles.add(new SimpleGrantedAuthority(user.getRole()));
         
-        UserContext userContext = new UserContext(user.get(), roles);
+        UserContext userContext = new UserContext(user, roles);
         
         return userContext;
     }
