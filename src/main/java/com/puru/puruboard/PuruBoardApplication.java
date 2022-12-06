3da4a2247e1e3d2bcf2955411fc501ca83f4acab
Choi Dasom <dasomchoi52@gmail.com>
2022-12-07 02:51:10 +0900

    Add @EnableJpaAuditing to use JpaAuditing

--- src/main/java/com/puru/puruboard/PuruBoardApplication.java
+++ src/main/java/com/puru/puruboard/PuruBoardApplication.java
@@ -2,12 +2,13 @@ package com.puru.puruboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PuruBoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PuruBoardApplication.class, args);
 	}
-
 }
