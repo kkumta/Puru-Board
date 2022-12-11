9c882a607741b67b9835d8c024bf5ae38ef0ed5a
Choi Dasom <dasomchoi52@gmail.com>
2022-12-12 02:07:15 +0900

    #2 - Add updatePostForm, updatePost method

--- src/main/java/com/puru/puruboard/controller/PostController.java
+++ src/main/java/com/puru/puruboard/controller/PostController.java
@@ -1,19 +1,27 @@
 package com.puru.puruboard.controller;
 
 import com.puru.puruboard.domain.Post;
+import com.puru.puruboard.domain.UserRepository;
 import com.puru.puruboard.dto.CreatePostDto;
 import com.puru.puruboard.dto.PostListResponseDto;
 import com.puru.puruboard.dto.PostResponseDto;
+import com.puru.puruboard.dto.UpdatePostDto;
 import com.puru.puruboard.service.PostService;
+import com.puru.puruboard.service.UserService;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -23,6 +31,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class PostController {
     
     private final PostService postService;
+    private final UserRepository userRepository;
     
     // 게시글 작성 폼
     @GetMapping("/posts/create")
@@ -35,9 +44,6 @@ public class PostController {
     public String createPost(@Valid @ModelAttribute CreatePostDto createPostDto,
                              RedirectAttributes redirectAttributes) {
         
-        System.out.println("createPostDto = " + createPostDto.getTitle());
-        System.out.println("createPostDto = " + createPostDto.getContent());
-        
         Long postId = postService.createPost(createPostDto);
         redirectAttributes.addAttribute("postId", postId);
         
@@ -45,8 +51,24 @@ public class PostController {
     }
     
     // 게시글 수정 폼
-    
+    @GetMapping("/posts/{postId}/update")
+    public String updatePostForm(@PathVariable Long postId, Model model) {
+        
+        PostResponseDto post = postService.findPost(postId);
+        model.addAttribute("post", post);
+        
+        return "post/update-post";
+    }
+
+
     // 게시글 수정
+    @PostMapping("/posts/{postId}/update")
+    public String updatePost(@PathVariable Long postId, @ModelAttribute UpdatePostDto updatePostDto) {
+    
+        postService.updatePost(postId, updatePostDto);
+        
+        return "redirect:/board/{postId}";
+    }
     
     // 게시글 삭제
     
@@ -55,15 +77,28 @@ public class PostController {
     public String readPost(@PathVariable Long postId, Model model) {
         PostResponseDto post = postService.findPost(postId);
         model.addAttribute("post", post);
+        model.addAttribute("user", false);
+    
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // authentication 검증
+        if (authentication != null && !AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
+            String curUserNickname = userRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName()).get().getNickname();
+            if (post.getAuthor().equals(curUserNickname)) {
+                model.addAttribute("user", true);
+            }
+        }
         
         return "post/post-info";
     }
     
-    // 게시글 목록 조회 -> 추후 페이징으로 변경 예정
+    // 게시글 목록 조회(자유 게시판) -> 추후 페이징으로 변경 예정
     @GetMapping("/board")
     public String readAllPosts(Model model) {
+        
         List<PostListResponseDto> posts = postService.findAllDesc();
+        
         model.addAttribute("posts", posts);
+        model.addAttribute("boardName", "자유 게시판");
         
         return "post/post-list";
     }
