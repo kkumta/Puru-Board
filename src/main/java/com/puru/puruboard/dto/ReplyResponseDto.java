03be65a1a558f604bfaf56c2b1031678ed7e4654
Choi Dasom <dasomchoi52@gmail.com>
2022-12-16 03:19:07 +0900

    #3 - Implement ReplyResponseDto for PostController
    * Add List<ReplyResponseDto> in attribute to show reply in post-info.html(e676e9b)

---
+++ src/main/java/com/puru/puruboard/dto/ReplyResponseDto.java
@@ -0,0 +1,23 @@
+package com.puru.puruboard.dto;
+
+import com.puru.puruboard.domain.Reply;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class ReplyResponseDto {
+    
+    private Long id;
+    private String author;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastEditDate;
+    
+    public ReplyResponseDto(Reply reply) {
+        this.id = reply.getId();
+        this.author = reply.getAuthor();
+        this.content = reply.getContent();
+        this.createdDate = reply.getCreatedDate();
+        this.lastEditDate = reply.getLastEditDate();
+    }
+}
