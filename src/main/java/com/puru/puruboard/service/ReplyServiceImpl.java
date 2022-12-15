c4cb5a7072c936e8746ec206aa185b6ae406b1db
Choi Dasom <dasomchoi52@gmail.com>
2022-12-16 05:36:51 +0900

    #3 - Implement ReplyServiceImpl to create reply
    * 댓글 작성 구현을 위해 ReplyServiceImpl에 createReply 메서드 추가
    * Implement createReply method in ReplyController(b69cf44)

--- src/main/java/com/puru/puruboard/service/ReplyServiceImpl.java
+++ src/main/java/com/puru/puruboard/service/ReplyServiceImpl.java
@@ -1,12 +1,17 @@
 package com.puru.puruboard.service;
 
+import com.puru.puruboard.domain.Post;
+import com.puru.puruboard.domain.PostRepository;
 import com.puru.puruboard.domain.Reply;
 import com.puru.puruboard.domain.ReplyRepository;
+import com.puru.puruboard.domain.UserRepository;
+import com.puru.puruboard.dto.CreateReplyDto;
 import com.puru.puruboard.dto.ReplyResponseDto;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReplyServiceImpl implements ReplyService {
     
     private final ReplyRepository replyRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
     
     @Override
     public List<ReplyResponseDto> findAllByPostId(Long postId) {
@@ -24,5 +31,26 @@ public class ReplyServiceImpl implements ReplyService {
         return replyList.stream().filter(r -> !r.getIsDeleted())
             .map(ReplyResponseDto::new)
             .collect(Collectors.toList());
+    }
+    
+    @Override
+    @Transactional
+    public Long createReply(Long postId, CreateReplyDto createReplyDto) {
+        String nickname = userRepository.findByEmail(
+            SecurityContextHolder.getContext().getAuthentication().getName()).get().getNickname();
+        
+        Optional<Post> post = postRepository.findById(postId);
+        if (post.isEmpty()) {
+            new Exception("존재하지 않는 게시물입니다.");
+            return null;
+        }
+        
+        Reply reply = Reply.builder()
+            .author(nickname)
+            .content(createReplyDto.getContent())
+            .post(post.get())
+            .build();
+        
+        return replyRepository.save(reply).getId();
     }
 }
