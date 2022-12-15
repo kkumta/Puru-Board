1638dc5d078b30a8f46e398af6f459163280464a
Choi Dasom <dasomchoi52@gmail.com>
2022-12-16 05:27:26 +0900

    #3 - Implement createReply method in ReplyController

---
+++ src/main/java/com/puru/puruboard/controller/ReplyController.java
@@ -0,0 +1,36 @@
+package com.puru.puruboard.controller;
+
+import com.puru.puruboard.dto.CreateReplyDto;
+import com.puru.puruboard.service.ReplyService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping
+public class ReplyController {
+    
+    private final ReplyService replyService;
+    
+    // 댓글 작성
+    @PostMapping("/posts/{postId}/reply/create")
+    public String createReply(@PathVariable Long postId,
+                              @Valid @ModelAttribute CreateReplyDto createReplyDto,
+                              RedirectAttributes redirectAttributes) {
+        
+        replyService.createReply(postId, createReplyDto);
+        redirectAttributes.addAttribute("postId", postId);
+        
+        return "redirect:/board/{postId}";
+    }
+    
+    // 댓글 수정
+    
+    // 댓글 삭제
+}
