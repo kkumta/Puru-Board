9f766e471a8f8274d83a80cf65f54e3a43ebce1b
Choi Dasom <dasomchoi52@gmail.com>
2022-12-19 01:21:46 +0900

    #3 - Add GetMapping deleteReply about 'delete reply' in ReplyController

--- src/main/java/com/puru/puruboard/controller/ReplyController.java
+++ src/main/java/com/puru/puruboard/controller/ReplyController.java
@@ -5,6 +5,7 @@ import com.puru.puruboard.service.ReplyService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,4 +34,15 @@ public class ReplyController {
     // 댓글 수정
     
     // 댓글 삭제
+    // 게시글 삭제
+    @GetMapping("/reply/{postId}/{replyId}/delete")
+    public String deleteReply(@PathVariable Long postId, @PathVariable Long replyId) {
+    
+        System.out.println("postId = " + postId);
+        System.out.println("replyId = " + replyId);
+        
+        replyService.deleteReply(replyId);
+    
+        return "redirect:/board/{postId}";
+    }
 }
